30bd0b348ee79936c24e62270967d84de3f080d5
Van5sh <vansh05dhir@gmail.com>
2025-02-13 02:33:54 +0530

    ui scrollbar

--- src/app/landingpage/land.tsx
+++ src/app/landingpage/land.tsx
@@ -3,35 +3,32 @@ import Navbar from "@/app/components/navbar";
 import Image from "next/image";
 
 
-
 export default function Land() {
-  return (
-    <>
+    return (
+        <>
 
-<div>
-<div className="relative h-screen">
+            <div>
+                <div className="relative h-screen">
 
-<img src="/images/dollars.jpg" alt="Dollars" className="w-full  h-full object-cover opacity-25 " />
+                    <img src="/images/dollars.jpg" alt="Dollars" className="w-full  h-full object-cover opacity-25 "/>
 
-<div className="absolute inset-0 text-5xl font-sans text-green-50  flex flex-col justify-center items-center gap-10 ">
-<div className="block">HOW TO GET</div>
-<div className="text-9xl font-semibold text-green-500">INSURED</div>
-</div>
+                    <div
+                        className="absolute inset-0 text-5xl font-sans text-green-50  flex flex-col justify-center items-center gap-10 ">
+                        <div className="block">HOW TO GET</div>
+                        <div className="text-9xl font-semibold text-green-500">INSURED</div>
+                    </div>
 
-<Navbar />
-</div>
-</div>
-<div className="p-10">
-<div className="text-5xl text-green-50 ">
-     BMI
-</div>
-</div>
-
-
-
+                    <Navbar/>
+                </div>
+            </div>
+            <div className="p-10">
+                <div className="text-5xl text-green-50 ">
+                    BMI
+                </div>
+            </div>
 
 
-{/* <div>
+            {/* <div>
   
   <div className="relative h-screen">
     
@@ -58,10 +55,6 @@ export default function Land() {
 </div> */}
 
 
-
-
-        
-
-    </>
-  );
+        </>
+    );
 }
