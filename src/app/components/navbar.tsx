08dfbbd72199f8f51cb665be0230bc4f5326f3eb
Van5sh <vansh05dhir@gmail.com>
2025-02-16 01:18:56 +0530

    blog

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -26,25 +26,27 @@ export default function Navbar() {
 
                     </>
                 ) : (
-                    <>
+                    <ul className="flex flex-row gap-10 justify-end items-center">
                         <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
                             Insurance Plans
                         </li>
                         <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
-                            Resources
+                            <Link href="/resources">
+                                Resources
+                            </Link>
                         </li>
                         <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
                             <Link href="#">
                                 <Info className="inline mr-2 mb-1" /> Contact Us
                             </Link>
                         </li>
-                        <li>
+                        <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full" >
                             Calculator
                         </li>
                         <button className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full" onClick={logOut}>
                             Log Out
                         </button>
-                    </>
+                    </ul>
                 )}
             </ul>
         </nav>
