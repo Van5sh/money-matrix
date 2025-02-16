d4b74445b3440490bb4dbb0341b2e640c76edc93
Van5sh <vansh05dhir@gmail.com>
2025-02-17 00:06:10 +0530

    blogs page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {House, ShieldCheck, Info, Boxes, Calculator,LogOut} from "lucide-react";
+import {House, ShieldCheck,Boxes, Calculator,LogOut} from "lucide-react";
 import Link from "next/link";
 import {UserAuth} from "@/app/context/AuthContext";
 
@@ -10,18 +10,13 @@ export default function Navbar() {
     return (
         <nav className="bg-green-600 p-6 text-white font-bold">
             <ul className="flex flex-row gap-10 justify-end items-center">
-                <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
-                    <Link href="/main">
-                        <House className="inline mr-2 mb-1"/> Home
-                    </Link>
-                </li>
-                <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
-                    <Link href="/about">
-                        <Info className="inline mr-2 mb-1"/> About Us
-                    </Link>
-                </li>
                 {user && (
                     <>
+                        <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
+                            <Link href="/main">
+                                <House className="inline mr-2 mb-1"/> Home
+                            </Link>
+                        </li>
                         <li className="relative pb-2 before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-white before:transition-all before:duration-500 hover:before:w-full">
                             <Link href="/insurance">
                                 <ShieldCheck className="inline mr-2 mb-1"/> Insurance Plans
