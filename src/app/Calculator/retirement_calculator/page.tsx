db51c265bf14620eea066dae445767123d27c7ec
Van5sh <vansh05dhir@gmail.com>
2025-02-27 14:50:14 +0530

    profile fix

--- src/app/Calculator/retirement_calculator/page.tsx
+++ src/app/Calculator/retirement_calculator/page.tsx
@@ -3,25 +3,27 @@
 import {useEffect, useState} from "react";
 
 export default function Page(){
-    const [age,setAge]=useState<number>();
+    const [age,setAge]=useState<number>(0);
     useEffect(() => {
         setAge(parseInt(sessionStorage.getItem("age") || "0"));
     }, []);
-    const [retirementAge,setRetirementAge]=useState<number>();
-    const [inflation,setInflation]=useState<number>();
+    const [retirementAge,setRetirementAge]=useState<number>(60);
+    const [inflation,setInflation]=useState<number>(1);
     const [expenses,setExpenses]=useState<number>();
     const [isOpen,setIsOpen]=useState<boolean>(false);
     const [yearlyExpense,setYearlyExpense]=useState<number>();
-    const [finalRate,setFinalRate]=useState<number>();
-    const [requiredAtRetirement,setRequiredAtRetirement]=useState<number>();
+    const [requiredAtRetirement,setRequiredAtRetirement]=useState<number>(0);
+    const [finalCost,setFinalCost]=useState<number>(0);
     const onSubmit = () => {
         if (expenses !== undefined && inflation !== undefined && age !== undefined && retirementAge !== undefined) {
             const expense = expenses * 12;
             setYearlyExpense(expense);
-            const rate = 1 + inflation / 100;
-            setFinalRate(rate);
-            const expenseAtRetirement = Math.pow((expenses * rate), retirementAge - age);
+            const rate = 1 + ((inflation )/ 100);
+            const time= retirementAge - age;
+            const expenseAtRetirement = expense*Math.pow(rate,time);
             setRequiredAtRetirement(expenseAtRetirement);
+            const final=(expenseAtRetirement/inflation)*100;
+            setFinalCost(final);
             setIsOpen(true);
         }
     };
@@ -30,19 +32,21 @@ export default function Page(){
             {/*current yearly expenses multiplied by the rate and raise to the power of the difference between the retirement age and the current age */}
             <form>
                 <p>Current Age</p>
-                <input value={age} onChange={(e)=>{setAge(e.target.valueAsNumber)}} className="border-2 rosunded-lg"/>
+                <input value={age} type="number" onChange={(e)=>{setAge(e.target.valueAsNumber)}} className="border-2 rosunded-lg"/>
                 <p>Retirement Age</p>
-                <input value={retirementAge} onChange={(e)=>{setRetirementAge(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
+                <input value={retirementAge} type="number" max={20} min={1} onChange={(e)=>{setRetirementAge(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
                 <p>Yearly Inflation</p>
-                <input value={inflation} onChange={(e)=>{setInflation(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
+                <input value={inflation} type="number" onChange={(e)=>{setInflation(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
                 <p>Monthly Expenses</p>
-                <input value={expenses} onChange={(e)=>{setExpenses(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
+                <input value={expenses} type="number" onChange={(e)=>{setExpenses(e.target.valueAsNumber)}} className="border-2 rounded-lg" />
             </form>
             <button onClick={onSubmit} className="bg-green-500 text-white">Calculate The money Required at your retirement</button>
             {
                 isOpen && (
                     <div>
-                        <p>Yearly Expense at Retirement:{yearlyExpense}</p>
+                        <p>Yearly Expense {yearlyExpense}</p>
+                        <p>{requiredAtRetirement.toFixed(2)}</p>
+                        <p>{finalCost}</p>
                     </div>
                 )
             }
