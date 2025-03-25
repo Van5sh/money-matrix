fb9e0dfbf8613a628b67f3f50d9b7fe4c905358e
Van5sh <vansh05dhir@gmail.com>
2025-03-26 02:37:15 +0530

    create blogs

--- src/app/resources/createblog/page.tsx
+++ src/app/resources/createblog/page.tsx
@@ -1,8 +1,39 @@
-const Createblog = () => {
+"use client";
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
+
+const Page = () => {
+    const router=useRouter();
+    const date=new Date();
     return (
-        <div>
-            <h1>Create Blog</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1>Create Your Blog</h1>
+            <Card className="flex flex-col space-y-4 min-w-[90vw] min-h-[90vh] bg-grey-500 border-green-900 border-8 rounded-lg shadow-lg">
+                <CardHeader className="bg-gray-400 text-xl font-bold">SHARE YOUR ADVICE WITH THE WORLD</CardHeader>
+                <CardContent className="flex flex-col space-y-4">
+                    <div className="flex flex-row space-x-8 justify-between">
+                        <label className="text-white justify-start ">Title: <input className="border-green-500 text-black border-4 rounded-lg"/></label>
+                        <label className="text-white justify-end">Description: <input className="bg-gray-200 text-black border-green-500 border-4 rounded-lg"/></label>
+                    </div>
+                        <label className="text-white flex flex-col">
+                            Description:
+                            <textarea className="border-green-500 text-black h-[50vh] border-4 rounded-lg"/>
+                        </label>
+                    <div className="flex flex-row justify-between">
+                        <label className="font-bold text-lg text-green-700">Created At: {date.toLocaleDateString()}</label>
+                        <div className="flex flex-row space-x-4">
+                            <Button className="bg-red-600 text-white font-bold" onClick={()=>{router.push("/resources")}} >
+                                Discard
+                            </Button>
+                            <Button onClick={()=>{router.push("/resources")}} className="bg-green-600 text-white font-bold">
+                                Post Blog
+                            </Button>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     );
 }
-export default Createblog;
+export default Page;
