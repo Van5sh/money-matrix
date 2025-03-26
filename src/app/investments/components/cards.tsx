8d9bf906b1b6241ca72461854dba3db9fcee9be4
Aditya <gjasunday@gmail.com>
2025-03-26 23:33:08 +0530

    investments first page  ui fixes

--- src/app/investments/components/cards.tsx
+++ src/app/investments/components/cards.tsx
@@ -7,15 +7,15 @@ import {useRouter} from "next/navigation";
 interface CardsProps {
     title: string;
     description: string;
-    link:string;
+    onclick:()=>void;
 }
 
-const Cards: React.FC<CardsProps> = ({ title, description,link }) => {
+const Cards: React.FC<CardsProps> = ({ title, description,onclick }) => {
     const router=useRouter();
     return(
         <Card
-            onClick={() => { router.push(`/${link}`) }}
-            className="flex border-green-900 flex-col justify-center items-center rounded-xl bg-gradient-to-r from-green-800 via-emerald-400 to-green-400 card-custom"
+            onClick={onclick}
+            className="flex border-green-900 border-2  h-[45vh] w-[45vh] mx-6 space-x-5 flex-col justify-center items-center rounded-xl  bg-gradient-to-b  from-blue-800 via-emerald-600 to-blue-400 card-custom"
         >
         <CardHeader className="flex justify-center items-center">
                     <CardTitle className="text-white pt-4 pb-2">{title}</CardTitle>
